c2b7c397fb662a52d73291942e90e2533a0b08cb
osaulo <osaulo_tarso@hotmail.com>
2019-05-09 23:37:22 -0300

    first commit.

---
+++ exercise1/README.md
@@ -0,0 +1,4 @@
+# algorithm
+1) Dado uma string, encontre o tamanho da maior substring (sem repetir caracteres)
+
+       ex.: "abbacdc" a saida deveria ser 4. (Favor enviar alguns testes unitários para que possamos validar o teu código).
---
+++ exercise2/undo_manager/src/undo/Change.java
@@ -0,0 +1,28 @@
+package undo;
+
+/**
+ * An abstraction of a change that can be registered with an
+ * {@link UndoManager} and applied to a {@link Document}. 
+ *
+ */
+public interface Change {
+	
+	/**
+	 * Apply this change to the given document.
+	 * 
+	 * @param doc The document to apply the change to.
+	 * @throws IllegalStateException If the change cannot be applied to <code>doc</code>
+	 * 			(that is if the document refuses the application of the change). 
+	 */
+	void apply(Document doc);
+
+	/**
+	 * Reverts this change in the given document.
+	 * 
+	 * @param doc The document to revert the change in.
+	 * @throws IllegalStateException If the change cannot be reverted in <code>doc</code>
+	 * 			(that is if the document refuses the reversion of the change). 
+	 */
+	void revert(Document doc);
+
+}
