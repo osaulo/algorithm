59e1602b48f63ddafae9a5e140f1a56c64c08573
osaulo <osaulo_tarso@hotmail.com>
2019-05-11 20:19:14 -0300

    exercicio 1 finalizado

--- exercise1/src/main/java/br/com/exercise/CalcSubStringLength.java
+++ exercise1/src/main/java/br/com/exercise/CalcSubStringLength.java
@@ -18,9 +18,33 @@ public class CalcSubStringLength {
 	
 	public int calc(String input) {
 		
-		// TODO 
+		// caracteres em processamento
+		StringBuilder inProcess = new StringBuilder();
 		
-		return 0;
+		// maior substring encontrada
+		String longestString = new String();
+		
+		for (int i = 0; i < input.length(); i++) {
+			char charAt = input.charAt(i);
+			
+			// se o caracter for encontrado,
+			// removo todos anterioes a ele, inclusive ele
+			int indexOf = inProcess.indexOf(String.valueOf(charAt));
+			if (indexOf >= 0) {
+				inProcess.delete(0, ++indexOf);
+			}
+			
+			// concatena os valores processados
+			inProcess.append(charAt);
+			
+			// verifica se o em processamento é maior que o armazenado
+			// se for positivo salvo a nova maior substring
+			if (inProcess.length() > longestString.length()) {
+				longestString = inProcess.toString();
+			}
+		}
+		
+		return longestString.length();
 	}
 
 }
--- exercise1/src/main/test/br/com/exercise/CalcSubStringLengthTest.java
+++ exercise1/src/main/test/br/com/exercise/CalcSubStringLengthTest.java
@@ -25,5 +25,53 @@ class CalcSubStringLengthTest {
 		
 		Assert.assertEquals(0, actual);
 	}
-
+	
+	@Test
+	public void testCalcStringOnlyOneChar() {
+		int actual = calcSubStringLength.calc("a");
+		
+		Assert.assertEquals(1, actual);
+	}
+	
+	@Test
+	public void testCalcStringOneCharRepeated() {
+		int actual = calcSubStringLength.calc("aaaaaaaaaaaaa");
+		
+		Assert.assertEquals(1, actual);
+	}
+	
+	@Test
+	public void testCalcStringOneCharRepeatedCaseSensitive() {
+		int actual = calcSubStringLength.calc("aAaAaAaAaAaAaA");
+		
+		Assert.assertEquals(2, actual);
+	}
+	
+	@Test
+	public void testCalcStringAllAlphabet() {
+		int actual = calcSubStringLength.calc("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		
+		Assert.assertEquals(26, actual);
+	}
+	
+	@Test
+	public void testCalcMoreOneSameSize3() {
+		int actual = calcSubStringLength.calc("abcabcabc");
+		
+		Assert.assertEquals(3, actual);
+	}
+	
+	@Test
+	public void testCalcMoreOneSameSize4() {
+		int actual = calcSubStringLength.calc("abbacdcabbacdc");
+		
+		Assert.assertEquals(4, actual);
+	}
+	
+	@Test
+	public void testCalcStringHappyCase() {
+		int actual = calcSubStringLength.calc("abbacdc");
+		
+		Assert.assertEquals(4, actual);
+	}
 }
